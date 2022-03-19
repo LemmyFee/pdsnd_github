2c12a05d6688e38c8bfe5c19edbc0e209dd0873f
Elemide Sofiyah Eniola <eniolaelemide@gmail.com>
2022-03-19 20:29:37 +0000

    Change docstrings message in get_filter function.

--- bikeshare.py
+++ bikeshare.py
@@ -7,6 +7,16 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'washington': 'washington.csv' }
 
 def get_filters():
+
+        """
+        Asks user to enter a city, month, and day to analyze.
+
+        Returns:
+            (str) city - name of the city to explore
+            (str) month - name of the month to filter by, or "all" to apply no month filter
+            (str) day - name of the day of week to filter by, or "all" to apply no day filter
+
+        """
     print('Hello! Let\'s explore some US bikeshare data! \n')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     cities = ["chicago", "washington", "new york city"]
